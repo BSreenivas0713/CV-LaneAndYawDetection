44fa1fbee849d00f8f4e89cafac27968b5c21cdd
Bharath Sreenivas <sreenivas.bharath@gmail.com>
2021-05-24 19:19:55 -0500

    added sobel mask, improved things

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import cv2
 import numpy as np 
 import math
 
-cap = cv2.VideoCapture('labeled/3.hevc')
+fileToUse = 1
+folderToUse = "labeled/" if fileToUse <= 5 else "unlabeled/"
+cap = cv2.VideoCapture(folderToUse + str(fileToUse) + ".hevc")
 # dataFile = open("labeled/3.txt")
 lines = []
-with open("labeled/3.txt") as f:
-    lines = [line.rstrip().split() for line in f]
+if fileToUse <= 5:
+    with open(folderToUse + str(fileToUse) + ".txt") as f:
+        lines = [line.rstrip().split() for line in f]
 
 i=0
 while cap.isOpened():
@@ -18,33 +21,48 @@ while cap.isOpened():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #getting white parts of img
-    equalized = cv2.equalizeHist(gray)
-    reg2, thresh = cv2.threshold(equalized, 250, 255, cv2.THRESH_BINARY)
-    # ret2, thresh = cv2.threshold(gray, 130, 145, cv2.THRESH_BINARY)
-    finalImg = cv2.bitwise_or(finalImg, thresh)
+    # equalized = cv2.equalizeHist(gray)
+    # reg2, thresh = cv2.threshold(equalized, 250, 255, cv2.THRESH_BINARY)
+    ret2, thresh2 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    # finalImg = cv2.bitwise_or(finalImg, thresh)
+    finalImg = cv2.bitwise_or(finalImg, thresh2)
 
     #getting yellow parts of img
-    yellow_HSV_th_min = (0,70,70)
+    yellow_HSV_th_min = (0,90,90)
     yellow_HSV_th_max = (50,255,255)
     HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     out = cv2.inRange(HSV, yellow_HSV_th_min,yellow_HSV_th_max)
     finalImg = cv2.bitwise_or(finalImg, out)
 
+    #getting sobel mask
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=9)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=9)
+
+    sobel_mag = np.sqrt(sobel_x ** 2 + sobel_y ** 2)
+    sobel_mag = np.uint8(sobel_mag / np.max(sobel_mag) * 255)
+
+    # 
+    # sobel_mag = sobel_mag.astype(np.uint)
+    sobel_mag = cv2.equalizeHist(sobel_mag)
+    _, sobel_mag = cv2.threshold(sobel_mag, 170, 255, cv2.THRESH_BINARY)
+    finalImg = cv2.bitwise_or(finalImg, sobel_mag)
+
     kernel = np.ones((5, 5), np.uint8)
     closing = cv2.morphologyEx(finalImg.astype(np.uint8), cv2.MORPH_CLOSE, kernel)
-    finalImg = cv2.bitwise_or(finalImg, closing)
+    # finalImg = cv2.bitwise_or(finalImg, closing)
     
-    yawRads = float(lines[i][1])
-    cv2.putText(finalImg, 
-                "radians: " + str(round(yawRads,4)), 
-                (50,50), cv2.FONT_HERSHEY_SIMPLEX, 
-                1, (255,255,255), 2, cv2.LINE_4)
-    cv2.putText(finalImg,
-                "degrees: " + str(round(yawRads*180/math.pi, 4)), 
-                (50,80), cv2.FONT_HERSHEY_SIMPLEX, 
-                1, (255,255,255), 2, cv2.LINE_4)
+    if fileToUse <= 5:
+        yawRads = float(lines[i][1])
+        cv2.putText(finalImg, 
+                    "radians: " + str(round(yawRads,4)), 
+                    (50,50), cv2.FONT_HERSHEY_SIMPLEX, 
+                    1, (255,255,255), 2, cv2.LINE_4)
+        cv2.putText(finalImg,
+                    "degrees: " + str(round(yawRads*180/math.pi, 4)), 
+                    (50,80), cv2.FONT_HERSHEY_SIMPLEX, 
+                    1, (255,255,255), 2, cv2.LINE_4)
 
-    cv2.imshow('frame', finalImg)
+    cv2.imshow('frame', sobel_mag)
     # cv2.moveWindow('frame', -1300,-600)
     cv2.moveWindow('frame', 100, -100)
 
